6c590cc3fcdc7b80f2e82d8c5b5fb5720faa0506
madihanazir <madihan541@gmail.com>
2025-07-29 18:11:15 +0530

    fix: use dynamic PORT for Railway

--- main.py
+++ main.py
@@ -140,5 +140,6 @@ def read_root():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))  # default to 8000 if running locally
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
 
