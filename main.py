b5bc3256ad9b31cf45c44c3dfbc575fd2a09d1d7
madihanazir <madihan541@gmail.com>
2025-07-30 23:44:26 +0530

    updated

--- main.py
+++ main.py
@@ -173,13 +173,13 @@ def read_root():
 
 print("DEBUG: FastAPI app initialized")
 
-# if __name__ == "__main__":
-#     import uvicorn
-#     import os
-#     port = int(os.environ.get("PORT", 8000))  
-#     uvicorn.run("main:app", host="0.0.0.0", port=port)
-#     print("DEBUG: MOCK_MODE =", MOCK_MODE)
-#     print("DEBUG: PORT =", os.getenv("PORT"))
+if __name__ == "__main__":
+    import uvicorn
+    import os
+    port = int(os.environ.get("PORT", 8000))  
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
+    print("DEBUG: MOCK_MODE =", MOCK_MODE)
+    print("DEBUG: PORT =", os.getenv("PORT"))
 
     
 # if __name__ == "__main__":
